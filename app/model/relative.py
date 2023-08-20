65785f18a4fd70cf5786ee7032a44c3edaec4ef2
MaciejMagic <maciej.madzik@gmail.com>
2023-08-20 13:02:22 +0200

    Fixes to relative class

--- app/model/relative.py
+++ app/model/relative.py
@@ -70,13 +70,16 @@ class FamilyRelative(Person):
     @first_name.setter
     def first_name(self, first_name: str) -> None:
         if first_name.isalpha() is False:
-            raise ValueError("Error: numeric characters in first name")
+            raise ValueError(
+                "Error > Assigning first name > Numeric characters in first name")
         if first_name is None:
             self._first_name = "<missing>"
         if len(first_name) < 2:
-            raise ValueError("Error: first name too short")
+            raise ValueError(
+                "Error > Assigning first name > First name too short")
         if len(first_name) > 14:
-            raise ValueError("Error: first name too long")
+            raise ValueError(
+                "Error > Assigning first name > First name too long")
         self._first_name = first_name
 
     @property
@@ -87,13 +90,16 @@ class FamilyRelative(Person):
     @last_name.setter
     def last_name(self, last_name: str) -> None:
         if last_name.isalpha() is False:
-            raise ValueError("Error: numeric characters in last name")
+            raise ValueError(
+                "Error > Assigning last name > Numeric characters in last name")
         if last_name is None:
             self._last_name = "<missing>"
         if len(last_name) < 2:
-            raise ValueError("Error: last name too short")
+            raise ValueError(
+                "Error > Assigning last name > Last name too short")
         if len(last_name) > 35:
-            raise ValueError("Error: last name too long")
+            raise ValueError(
+                "Error > Assigning last name > Last name too long")
         self._last_name = last_name
 
     @property
@@ -104,25 +110,28 @@ class FamilyRelative(Person):
     @gender.setter
     def gender(self, gender: str) -> None:
         if gender.strip().lower() not in ['female', 'male']:
-            raise ValueError("Error: gender must be 'female' or 'male'")
+            raise ValueError(
+                "Error > Assigning gender > Gender must be 'female' or 'male'")
         self._gender = gender
 
     @property
     def family_name(self) -> str | None:
         """ Returns objects family last name """
-        return self._family_name
+        return self.family_data.family_name
 
     @family_name.setter
     def family_name(self, family_name: str) -> None:
         # No alphabetic validation - to allow regnal / family numbers
         if family_name:
             if len(family_name) < 2:
-                raise ValueError("Error: Family name too short")
+                raise ValueError(
+                    "Error > Assigning family name > Family name too short")
             if len(family_name) > 35:
-                raise ValueError("Error: Family name too long")
-            self._family_name = family_name
+                raise ValueError(
+                    "Error > Assigning family name > Family name too long")
+            self.family_data.family_name = family_name
         else:
-            self._family_name = None
+            self.family_data.family_name = None
 
     @property
     def date_of_birth(self) -> str | None:
@@ -138,7 +147,7 @@ class FamilyRelative(Person):
         else:
             self._date_of_birth = None
             raise ValueError(
-                "Error: Invalid date format. Must be 'YYYY-MM-DD'")
+                "Error > Assigning Date of Birth > Invalid date format (Must be 'YYYY-MM-DD')")
 
     @property
     def place_of_birth(self) -> str | None:
@@ -149,13 +158,16 @@ class FamilyRelative(Person):
     def place_of_birth(self, place_of_birth: str) -> None:
         if place_of_birth:
             if len(place_of_birth) < 2:
-                raise ValueError("Error: city name too short")
+                raise ValueError(
+                    "Error > Assigning place of birth > City name too short")
             if len(place_of_birth) > 60:
-                raise ValueError("Error: city name too long")
+                raise ValueError(
+                    "Error > Assigning place of birth > City name too long")
             self._place_of_birth = place_of_birth
         else:
             self._place_of_birth = None
-            raise ValueError("Error: city name value empty")
+            raise ValueError(
+                "Error > Assigning place of birth > City name value empty")
 
     @property
     def date_of_death(self) -> str | None:
@@ -171,7 +183,7 @@ class FamilyRelative(Person):
         else:
             self._date_of_death = None
             raise ValueError(
-                "Error: Invalid date format. Must be 'YYYY-MM-DD'")
+                "Error > Assigning date of death > Invalid date format (Must be 'YYYY-MM-DD')")
 
     @property
     def place_of_death(self) -> str | None:
@@ -182,76 +194,80 @@ class FamilyRelative(Person):
     def place_of_death(self, place_of_death: str) -> None:
         if place_of_death:
             if len(place_of_death) < 2:
-                raise ValueError("Error: city name too short")
+                raise ValueError(
+                    "Error > Assigning place of death > City name too short")
             if len(place_of_death) > 60:
-                raise ValueError("Error: city name too long")
+                raise ValueError(
+                    "Error > Assigning place of death > City name too long")
             self._place_of_death = place_of_death
         else:
             self._place_of_death = None
-            raise ValueError("Error: city name value empty")
+            raise ValueError(
+                "Error > Assigning place of death > City name value empty")
 
     @property
     def phone(self) -> str | None:
         """ Returns this persons phone number """
-        return self._phone
+        return self.person_data.phone
 
     @phone.setter
     def phone(self, phone: str) -> None:
         # Check if phone number string has at least 7 digits
         if len(filter(str.isdigit, phone)) > 6 and len(filter(str.isdigit, phone)) < 16:
-            self._phone = phone
+            self.person_data.phone = phone
         else:
-            self._phone = None
+            self.person_data.phone = None
             raise ValueError(
-                "Error: Phone number must be numerical and min. 7 digits long")
+                "Error > Assigning phone number > Must be numerical value and min. 7 digits long")
 
     @property
     def email(self) -> str | None:
         """ Returns this persons email address """
-        return self._email
+        return self.person_data.email
 
     @email.setter
     def email(self, email_address: str) -> None:
         if email_address == "delete":
-            self._email = None
+            self.person_data.email = None
         if checkers.is_email(email_address):
-            self._email = validators.email(email_address, allow_empty=True)
+            self.person_data.email = validators.email(
+                email_address, allow_empty=True)
         else:
-            self._email = None
-            raise ValueError("Invalid email address")
+            self.person_data.email = None
+            raise ValueError("Error > Assigning email > Invalid address")
 
     @property
     def events(self) -> list[str] | None:
         """ Returns this persons saved events """
-        if not self._events:
-            print("Events are empty")
+        if not self.person_data.events:
+            print("Event list is empty")
             return None
-        return self._events
+        return self.person_data.events
 
     @events.setter
     def events(self, event) -> None:
-        self._events.append(event)
+        self.person_data.events.append(event)
 
     @property
     def desc(self) -> list[str] | None:
         """ Returns this persons description """
-        if not self._desc:
+        if not self.person_data.desc:
             print("Description is empty")
             return None
-        return self._desc
+        return self.person_data.desc
 
     @desc.setter
     def desc(self, desc) -> None:
-        self._desc.append(desc)
+        self.person_data.desc.append(desc)
 
     @property
     def children(self) -> list[int] | None:
         """ Returns this persons children list """
-        return self._children
+        return self.family_data.children
 
     @children.setter
     def children(self, children: int | list[int]) -> None:
-        self._children.append(children)
+        self.family_data.children.append(children)
 
     # Methods
 
@@ -282,35 +298,42 @@ class FamilyRelative(Person):
 
     def event_add(self, event: str) -> None:
         """ Add new event to persons bio """
-        self._events.append(event)
+        self.person_data.events.append(event)
 
     def desc_add(self, desc: str) -> None:
         """ Add new description to persons bio """
-        self._desc.append(desc)
+        self.person_data.desc.append(desc)
 
     def marry(self, status: bool = True) -> None:
         """ Change married status for this person """
-        if self.married is True:
-            print("Status not changed: already married", file=sys.stderr)
-        self.married = status
+        if self.family_data.married is True:
+            print("Error > Status not changed > Already married", file=sys.stderr)
+        self.family_data.married = status
 
     def set_mother(self, mother_id) -> None:
         """ Sets mother relative for this person """
         if int(mother_id):
-            self.mother = mother_id
+            self.family_data.mother = mother_id
         else:
-            raise ValueError("Error setting mother id: not an integer")
+            raise ValueError("Error > Assigning mother id > Not an integer")
 
     def set_father(self, father_id) -> None:
         """ Sets father relative for this person """
         if int(father_id):
-            self.father = father_id
+            self.family_data.father = father_id
         else:
-            raise ValueError("Error setting father id: not an integer")
+            raise ValueError("Error > Assigning father id > Not an integer")
 
     def set_spouse(self, spouse) -> None:
         """ Sets spouse relative for this person """
         if int(spouse):
-            self.spouse_current = spouse
+            self.family_data.spouse_current = spouse
         else:
-            raise ValueError("Error setting spouse id: not an integer")
+            raise ValueError("Error > Assigning spouse id > Not an integer")
+
+    def child_add(self, child) -> None:
+        """ Adds a child to Persons list of children """
+        if int(child):
+            self.family_data.children.append(child)
+        else:
+            raise ValueError("Error > Adding child > Invalid value")
